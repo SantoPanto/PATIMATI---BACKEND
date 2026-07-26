ffe921a2ff7949dfc287e6fe2c8798e773cbfd0c
madineyusuf <github_yusvpovagmail.com>
2026-07-26 16:09:39 +0300

    fix: @ GIST ve geography

--- src/main/java/com/works/patimati/entity/Ad.java
+++ src/main/java/com/works/patimati/entity/Ad.java
@@ -5,7 +5,12 @@ import lombok.*;
 import org.locationtech.jts.geom.Point;
 
 @Entity
-@Table(name = "ads")
+@Table(
+        name = "ads",
+        indexes = {
+                @Index(name = "idx_ads_location", columnList = "location") // GIST İndeksi (3. Kısım)
+        }
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -23,7 +28,7 @@ public class Ad {
     @Enumerated(EnumType.STRING)
     private AdType adType; // LOST, FOUND, ADOPTION
 
-    // KISIM 3: Coğrafi Konum
+    // KISIM 3: Coğrafi Konum (GIS)
     @Column(columnDefinition = "geometry(Point, 4326)")
     private Point location;
 
