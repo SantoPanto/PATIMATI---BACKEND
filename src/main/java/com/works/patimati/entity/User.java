99980eb3e25a81e68804783e627f2a004cf7eb47
Fatih Şahin <fatihpanto@gmail.com>
2026-08-04 13:41:58 +0300

    Remove comments for uid field in User class
    Removed commented documentation for uid field.

--- src/main/java/com/works/patimati/entity/User.java
+++ src/main/java/com/works/patimati/entity/User.java
@@ -15,11 +15,6 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    /*
-     * Java tarafındaki uid alanı, veritabanındaki users.id
-     * primary key kolonuna karşılık gelir.
-     */
-    @Column(name = "id")
     private Long uid;
 
     @Column(name = "google_id", unique = true)
