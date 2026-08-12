4d733f1c5f46c5ef492bc3229d41865cd4ea9d57
madineyusuf <github_yusvpovagmail.com>
2026-08-12 19:07:45 +0300

    PdfPosterService, QrCodeService, poster endpoint

--- src/main/java/com/works/patimati/controller/AdController.java
+++ src/main/java/com/works/patimati/controller/AdController.java
@@ -32,6 +32,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.works.patimati.dto.ad.ResolveLostAdRequest;
 
 import java.net.URI;
 import java.util.List;
