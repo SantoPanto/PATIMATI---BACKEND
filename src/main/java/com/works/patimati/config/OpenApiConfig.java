bef656c8e0a5aff08963a8d8cc21c2dd611905ea
SantoPanto <fatihpanto@gmail.com>
2026-08-19 19:22:26 +0300

    Bağımlılık sürüm güncellemeleri yapıldı. OpenApi konfigirasyonu ile SecurityScheme eklendi. Application ve spring yapılandırmalarında local testler başarıya ulaştı

---
+++ src/main/java/com/works/patimati/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.works.patimati.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    public static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PatiMati API Dokümantasyonu")
+                        .version("1.0.0")
+                        .description("PatiMati Evcil Hayvan Sahiplendirme ve Kayıp İlan Platformu REST API Dokümantasyonu")
+                        .contact(new Contact()
+                                .name("PatiMati Ekibi")
+                                .email("info@patimati.me"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Lütfen 'Bearer ' öneki olmadan geçerli JWT token'ınızı giriniz.")));
+    }
+}
