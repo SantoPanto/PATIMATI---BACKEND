333be27d1b0a605bcdbe2d9a81326c02cdd1fc1f
SantoPanto <fatihpanto@gmail.com>
2026-08-20 11:15:34 +0300

    Yapaay zeka enumları güncellendi ve uyumlu hale getirildi

--- src/main/java/com/works/patimati/entity/enums/AiStatus.java
+++ src/main/java/com/works/patimati/entity/enums/AiStatus.java
@@ -1,10 +1,11 @@
 package com.works.patimati.entity.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Yapay zeka analiz durumu", allowableValues = {"PENDING", "DONE", "FAILED", "NOT_APPLICABLE"})
 public enum AiStatus {
     PENDING,
     DONE,
     FAILED,
-    APPROVED,
-    REJECTED,
     NOT_APPLICABLE
 }
